967b9b0ca4915b4a7369db976a43ad8f5129473c
OChebanenko <acebanenco@gmail.com>
2023-08-04 08:51:22 +0200

    #11 add JSON schema for attributes and mappings

--- data-sync-service/src/main/java/com/github/zybercik00/DataInitializerService.java
+++ data-sync-service/src/main/java/com/github/zybercik00/DataInitializerService.java
@@ -26,56 +26,5 @@ public class DataInitializerService {
 
     @PostConstruct
     public void initializeData() {
-        List<String> mappingPaths = Arrays.asList(
-                "material.name",
-                "preparedOn",
-                "weightBefore",
-                "weightAfter",
-                "waste.lossAfterExtractionInKg",
-                "waste.lossAfterExtractionInPercent",
-                "receivedBackOn",
-                "sampleTestResult",
-                "waste.packedKg",
-                "waste.lossTotalKg",
-                "waste.lossTotalPercents"
-        );
-
-        for (String path : mappingPaths) {
-            MappingEntity mappingAttribute = new MappingEntity();
-            mappingAttribute.setSource(path);
-            mappingRepo.save(mappingAttribute);
-        }
-
-        List<Map<String, String>> referenceMappings = Arrays.asList(
-                Map.of("targetProperty", "material", "nestedProperty", "lot"),
-                Map.of("targetProperty", "realizedBy", "nestedProperty", "name")
-        );
-
-        for (Map<String, String> mapping: referenceMappings) {
-            ReferenceAttributeEntity referenceEntity = new ReferenceAttributeEntity();
-            referenceEntity.setTargetProperty(mapping.get("targetProperty"));
-            referenceEntity.setNestedProperty(mapping.get("nestedProperty"));
-            referenceMappingAttributeRepo.save(referenceEntity);
-
-        }
-
-        List<Map<String, String>> qualifierMappings = Arrays.asList(
-                Map.of("targetProperty", "purchasePrices", "qualifierProperty", "purchasePrice",
-                        "qualifierParent", "extraction", "qualifier", "{\"currency\":{\"code\":\"EUR\"}}")
-        );
-
-        for (Map<String, String> mapping: qualifierMappings) {
-            QualifierAttributeEntity qualifierEntity = new QualifierAttributeEntity();
-            qualifierEntity.setTargetProperty(mapping.get("targetProperty"));
-            qualifierEntity.setQualifierProperty(mapping.get("qualifierProperty"));
-            qualifierEntity.setQualifierParent(mapping.get("qualifierParent"));
-            try {
-                qualifierEntity.setQualifier(objectMapper.writeValueAsString(mapping.get("qualifier")));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            attributeRepo.save(qualifierEntity);
-        }
-
     }
 }
