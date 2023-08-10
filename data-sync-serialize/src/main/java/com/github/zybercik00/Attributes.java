4084feadd38d9ff306864bc37063a66e5b69bcb7
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-08-10 12:58:42 +0200

    #11 Refactoring Attributes class fields from object to list of object

--- data-sync-serialize/src/main/java/com/github/zybercik00/Attributes.java
+++ data-sync-serialize/src/main/java/com/github/zybercik00/Attributes.java
@@ -3,10 +3,12 @@ package com.github.zybercik00;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Attributes {
-    private JsonQualifiedAttribute jsonQualifiedAttribute;
-    private JsonReferencedAttribute jsonReferencedAttribute;
-    private JsonSimpleAttribute jsonSimpleAttribute;
+    private List<QualifiedAttributeJson> qualified;
+    private List<ReferencedAttributeJson> reference;
+    private List<SimpleAttributeJson> simple;
 }
