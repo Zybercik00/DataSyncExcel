aada219d56fe6522befd57b9d1920e26915ae3c2
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-08-17 23:02:57 +0200

    #11 Pruned unnecessary classes.

--- data-sync-repository/src/main/java/com/github/zybercik00/repository/process/PriceRepo.java
+++
@@ -1,7 +0,0 @@
-package com.github.zybercik00.repository.process;
-
-import com.github.zybercik00.domain.process.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepo extends JpaRepository<Price, String> {
-}
