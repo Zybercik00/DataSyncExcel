2c898004b978bbf07c166174cdf12ae80a25ad84
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-08-22 15:39:35 +0200

    #11 Fixed the bug by converting the method's result from a simple object to a list of objects

--- data-sync-init/src/main/java/com/github/zybercik00/JsonMappingLoader.java
+++ data-sync-init/src/main/java/com/github/zybercik00/JsonMappingLoader.java
@@ -6,38 +6,46 @@ import com.github.zybercik00.domain.metadata.SimpleAttributeEntity;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JsonMappingLoader {
 
     private final JsonAttributeLoader jsonAttributeLoader;
 
-    ReferenceAttributeEntity loadReferenced() throws IOException {
-        ReferenceAttributeEntity referenceAttributeEntity = new ReferenceAttributeEntity();
+    List<ReferenceAttributeEntity> loadReferenced() throws IOException {
+        List<ReferenceAttributeEntity> referenceAttributeEntityList = new ArrayList<>();
         for (ReferencedAttributeJson referencedAttributeJson : jsonAttributeLoader.load().getReference()) {
+            ReferenceAttributeEntity referenceAttributeEntity = new ReferenceAttributeEntity();
             referenceAttributeEntity.setPath(referencedAttributeJson.getPath());
             referenceAttributeEntity.setTargetProperty(referencedAttributeJson.getTargetProperty());
             referenceAttributeEntity.setNestedProperty(referencedAttributeJson.getNestedProperty());
+            referenceAttributeEntityList.add(referenceAttributeEntity);
         }
-        return referenceAttributeEntity;
+        return referenceAttributeEntityList;
     }
-    SimpleAttributeEntity loadSimple() throws IOException {
-        SimpleAttributeEntity simpleAttributeEntity = new SimpleAttributeEntity();
+    List<SimpleAttributeEntity> loadSimple() throws IOException {
+        List<SimpleAttributeEntity> simpleAttributeEntityList = new ArrayList<>();
         for (SimpleAttributeJson simpleAttributeJson :  jsonAttributeLoader.load().getSimple()) {
+            SimpleAttributeEntity simpleAttributeEntity = new SimpleAttributeEntity();
             simpleAttributeEntity.setPath(simpleAttributeJson.getPath());
             simpleAttributeEntity.setTargetProperty(simpleAttributeJson.getTargetProperty());
+            simpleAttributeEntityList.add(simpleAttributeEntity);
         }
-        return simpleAttributeEntity;
+        return simpleAttributeEntityList;
     }
-    QualifiedAttributeEntity loadQualified() throws IOException {
-        QualifiedAttributeEntity qualifiedAttributeEntity = new QualifiedAttributeEntity();
+    List<QualifiedAttributeEntity> loadQualified() throws IOException {
+        List<QualifiedAttributeEntity> qualifiedAttributeEntityList = new ArrayList<>();
         for (QualifiedAttributeJson qualifiedAttributeJson : jsonAttributeLoader.load().getQualified()) {
+            QualifiedAttributeEntity qualifiedAttributeEntity = new QualifiedAttributeEntity();
             qualifiedAttributeEntity.setPath(qualifiedAttributeJson.getPath());
             qualifiedAttributeEntity.setTargetProperty(qualifiedAttributeJson.getTargetProperty());
             qualifiedAttributeEntity.setQualifiedProperty(qualifiedAttributeJson.getQualifierProperty());
             qualifiedAttributeEntity.setQualifiedParent(qualifiedAttributeJson.getQualifierParent());
             qualifiedAttributeEntity.setQualifier(qualifiedAttributeJson.getQualifier().toString());
+            qualifiedAttributeEntityList.add(qualifiedAttributeEntity);
         }
-        return qualifiedAttributeEntity;
+        return qualifiedAttributeEntityList;
     }
 }
--- data-sync-model/src/main/java/com/github/zybercik00/domain/process/Waste.java
+++ data-sync-model/src/main/java/com/github/zybercik00/domain/process/Waste.java
@@ -47,7 +47,7 @@ public class Waste {
     @Column(name = "LOSS_TOTAL_PERCENTS")
     private BigDecimal lossTotalPercents;
 
-    @OneToOne(fetch = FetchType.EAGER)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "EXTRACTION_ID",
             nullable = false,
             foreignKey = @ForeignKey(name = "FK_WST_EXT"))
